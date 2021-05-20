bcab2ba7ca9fef15af5e6545468d47a6a9926aa8
jeffgarrett <jeff2invest@gmail.com>
2021-05-20 09:02:16 -0700

    different way to do

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { Webhooks } = require('@octokit/webhooks');
 const fs = require('fs');
 
 //setup brnahcing structure
-let testFile = fs.readFileSync('branchStructure.json');
-let testBranchStructure = JSON.parse(testFile);
-//const token = core.getInput('github-token', { required: true })
-
-let branchStructure = core.getInput('branch_structure') || testBranchStructure
+let branchRulesFile = fs.readFileSync('branchStructure.json');
+let branchRules = JSON.parse(branchFile);
+const token = (core.getInput('github_token') || process.env.GITHUB_TOKEN);
+let branchStructure = core.getInput('branch_structure') || branchRules;
+const octokit = new github.GitHub(token)
+const context = github.context
 
 console.log("testing again");
 
 try {
+    const pull_number = parseInt(core.getInput('pull_number'),0);
     const currentBranch = github.context.ref;
     const prPayload = github.context.payload.pull_request;
+    
+    const request = await octokit.pulls.get({
+        ...context.repo,
+        pull_number
+      })
+    
+    const pr = request.data;
+    
+    console.log("The base branch is: " + pr.base.ref);
+    console.log("The head branch is: " + pr.head.ref);
+    console.log(JSON.stringify("The Branch strucutre is : \n" + branchStructure))
     
     console.log(currentBranch);
     console.log(prPayload.number);
 
-    /* `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`); */
+
 } catch (error) {
 
     core.setFailed(error.message);
