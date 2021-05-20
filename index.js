98bc8335d751c29045ce99f759b904097be36629
jeffgarrett <jeff2invest@gmail.com>
2021-05-20 12:34:31 -0700

    testing functionality

--- index.js
+++ index.js
@@ -10,28 +10,24 @@ const branchStructure = core.getInput('branch_structure') || branchRules;
 
 //setup rest client
 const token = (core.getInput('github_token') || process.env.GITHUB_TOKEN);
+
+//environment variables that could be used vs API call.
 const base = (core.getInput('github_base_ref') || process.env.GITHUB_BASE_REF);
 const head = (core.getInput('github_head_ref') || process.env.GITHUB_HEAD_REF);
-
-console.log("base: " + base);
-console.log("head: " + head);
-console.log("context head" + github.context.baseRef);
-console.log("context head" + github.context.base_ref);
 
 const octokit = github.getOctokit(token);
 const context = github.context
 
 async function run() {
-    console.log("starting try catch");
 
     try {
-        const currentBranch = context.ref;
+
         const prPayload = github.context.payload.pull_request.number;
-        const repo = process.env.GITHUB_REPO;
-        console.log("current branch is: " + currentBranch);
+
+        ;
         console.log("prnumber is: " + prPayload);
         console.log("current repo: " + context.repo.repo);
-        console.log("current repo: " + repo);
+
 
         const request = await octokit.pulls.get({
             owner: context.repo.owner,
@@ -41,12 +37,30 @@ async function run() {
 
         const pr = request.data;
 
-        console.log("The base branch is: " + pr.base.ref);
-        console.log("The head branch is: " + pr.head.ref);
-        console.log(JSON.stringify(branchStructure));
+        branchRules.branchRules.forEach(branch => {
 
-        
-        console.log(prPayload.number);
+            if (branch.branch == pr.base.ref) {
+                branch.accepted_incoming_branches.forEach(rule => {
+                    if (rule == pr.head.ref) {
+                        console.log("This is ok!");
+                        core.ExitCode.Success;
+
+                    }
+                });
+
+            } else {
+                console.log("The base branch is: " + pr.base.ref);
+                console.log("The head branch is: " + pr.head.ref);
+                console.log(JSON.stringify(branchStructure));
+                console.log(prPayload.number);
+                core.setFailed("No matching branch rule for merging " + pr.head.ref + " into " + pr.base.ref);
+                core.ExitCode.Failure;
+            }
+
+        });
+
+
+
 
 
     } catch (error) {
